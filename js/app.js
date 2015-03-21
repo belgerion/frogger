d896bc3aff44d57ff98368b2cc9c783895f60a1e
belgerion <belgerion@gmail.com>
2015-03-21 16:05:53 -0600

    update

--- js/app.js
+++ js/app.js
@@ -25,14 +25,14 @@ var Enemy = function(sideways,updown) {
     // This method was chosen based on the fact that there was only one variable, the position on the x-axis, that was important for the roaches to be able to move. Once it was off the edge of the canvas, it would then be reset by the else statement, to be ready to cross again.
         if (this.x<820) { this.x+=(200*dt); }
         else {this.x=-140;}
-    }
+    };
 
 // Draw the enemy on the screen, required method for game
     Enemy.prototype.render = function() {
         ctx.drawImage(Resources.get(this.sprite),
             this.x,
             this.y);
-    }
+    };
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -56,7 +56,7 @@ var Player = function(sideways,updown) {
                 allEnemies[roach].x + 50 > this.x + 15&&
                 allEnemies[roach].y === this.y)
                 {player.reset();}
-        };
+        }
         if(this.y === -10){player.reset();}
     };
 //Used the Enemy.prototype.render method that was provided to render the player on the convas, just renaming it to support the Player class.
@@ -65,7 +65,7 @@ var Player = function(sideways,updown) {
         ctx.drawImage(Resources.get(this.sprite),
             this.x,
             this.y);
-    }
+    };
 
 // The reset method was set up to place the player in the middle of the first row of grass on the canvas, using the this keyword to set the correct position. It was also modeled on the Enemy.prototype.render method, just removing the rendering from that function, while maintaining the this structure., assigning them values. Additionally, should at some point a second instance of the Player be created, each would be able to take in different values.
     Player.prototype.reset = function() {
@@ -77,7 +77,7 @@ var Player = function(sideways,updown) {
         if (input == "up" && this.y >25) {this.y = this.y - 40;}
         if (input == "down" && this.y < 350) {this.y = this.y + 40;}
         if (input == "left" && this.x > 0) {this.x = this.x -50;}
-        if (input == "right" && this.x < 400) {this.x = this.x +50}
+        if (input == "right" && this.x < 400) {this.x = this.x +50;}
     };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
